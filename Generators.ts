71c790a1a5569e424fea302499ae669c73cc176e
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-09-01 12:50:08 -0600

    Generators otro ejemplo de argumentos

--- Generators.ts
+++ Generators.ts
@@ -1,10 +1,10 @@
-function* generator() {
-    var bar = yield 'Console log';
-    console.log(bar);
-    yield 1;
-}.
-const iterator = generator();
-const foo = iterator.next();
-console.log(foo.value);
-const nextThing = iterator.next('Un texto inyectado');
-console.log(nextThing);
+function* logGenerator() {
+    console.log(yield);
+    console.log(yield);
+    console.log(yield);
+    }
+    var gen = logGenerator();
+    gen.next();
+    gen.next('pretzel');
+    gen.next('california');
+    gen.next('mayonnaise');
