017278c70aadb2a250d7c487eb746b93c974a5dd
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-09-01 02:33:16 -0600

    Promesas (Ejemplo de llamada a catch)

--- Promesas.ts
+++ Promesas.ts
@@ -1,9 +1,9 @@
 const promise = new Promise((resolve, reject) => {
-    resolve(123);
+    reject(new Error("Algo malo a pasado"));
 });
 promise.then((res) => {
-    console.log('I get called:', res === 123);
+    
 });
 promise.catch((err) => {
-
+    console.log('Tengo una llamada: ', err.message);
 });
