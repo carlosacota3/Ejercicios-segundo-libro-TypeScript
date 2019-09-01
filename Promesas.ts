58ca5317eed5cc966b215a5315ea1ec8f715ca64
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-09-01 03:24:47 -0600

    Carga de un JSON de forma asíncrona

--- Promesas.ts
+++ Promesas.ts
@@ -1,12 +1,24 @@
-function iReturnPromiseAfter1Second(): Promise<string> {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve("Hello world!"), 1000);
-    });
-}
-Promise.resolve(123)
-    .then((res) => {
-        return iReturnPromiseAfter1Second();
+// good json file
+loadJSONAsync('good.json')
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('good.json error', err.message);
     })
-    .then((res) => {
-        console.log(res);
+
+    // no existe json file
+    .then(function () {
+        return loadJSONAsync('absent.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('absent.json error', err.message);
+    })
+
+    // invalid json file
+    .then(function () {
+        return loadJSONAsync('bad.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('bad.json error', err.message);
     });
