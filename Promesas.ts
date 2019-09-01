29cb43b7c6be6c0debd1b66edf1eac35ae1e4754
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-09-01 03:11:43 -0600

    Un método catch que continue con la cadena de promesas

--- Promesas.ts
+++ Promesas.ts
@@ -3,14 +3,10 @@ Promise.reject(new Error('something bad happened'))
         console.log(res);
         return 456;
     })
-    .then((res) => {
-        console.log(res);
-        return Promise.resolve(123);
-    })
-    .then((res) => {
-        console.log(res);
-        return Promise.resolve(123);
-    })
     .catch((err) => {
         console.log(err.message);
-    });
+        return Promise.resolve(123);
+    })
+    .then((res) => {
+        console.log(res);
+    })
